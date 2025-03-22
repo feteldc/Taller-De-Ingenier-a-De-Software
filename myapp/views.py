6bc8b63cb14f57e6a2f30852cc3f72cb6264479d
feteldc <fercatalansilva@gmail.com>
2025-03-22 18:15:38 -0300

    si

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def hello(request):
+    return HttpResponse("hello word")
+
+def about(request):
+    return HttpResponse('about')
